aa40cb87ca9b1c5894e811dc3e00644de3137397
Miguel <gomezmig03@gmail.com>
2026-03-30 15:26:00 +0200

    Temporary fix to updates

--- hypr/scripts/KooLsDotsUpdate.sh
+++ hypr/scripts/KooLsDotsUpdate.sh
@@ -1,33 +1,34 @@
 #!/bin/bash
 # /* ---- 💫 https://github.com/JaKooLit 💫 ---- */  ##
 # simple bash script to check if update is available by comparing local version and github version
-
 # Local Paths
 local_dir="$HOME/.config/hypr"
 iDIR="$HOME/.config/swaync/images/"
 local_version=$(ls $local_dir/v* 2>/dev/null | sort -V | tail -n 1 | sed 's/.*v\(.*\)/\1/')
-KooL_Dots_DIR="$HOME/Hyprland-Dots"
 
 # exit if cannot find local version
 if [ -z "$local_version" ]; then
-  notify-send -i "$iDIR/error.png" "ERROR "!?!?!!"" "Unable to find KooL's dots version . exiting.... "
+  notify-send -i "$iDIR/error.png" "ERROR "!"" "Unable to find KooL's dots version . exiting.... "
   exit 1
 fi
 
 # GitHub URL - KooL's dots
-branch="main"
-github_url="https://github.com/JaKooLit/Hyprland-Dots/tree/$branch/config/hypr/"
+#github_commits=$(curl -s "https://api.github.com/repos/GomezMig03/dotfiles/commits?per_page=1" -I | grep -o 'page=[0-9]*' | tail -1 | cut -d= -f2)
+#local_commits=$(cd $HOME/.config/ && git log --author="gomezmig03@gmail.com" --format="%ad" --date=short -1)
+local_date=$(cd $HOME/.config/ && git log --author="gomezmig03@gmail.com" --format="%ad" --date=short -1)
 
-# Fetch the version from GitHub URL - KooL's dots
-github_version=$(curl -s $github_url | grep -o 'v[0-9]\+\.[0-9]\+\.[0-9]\+' | sort -V | tail -n 1 | sed 's/v//')
+github_date=$(curl -s "https://api.github.com/repos/GomezMig03/dotfiles/commits?author=GomezMig03&per_page=1" \
+  | grep -o '"date": *"[^"]*"' | head -1 | cut -d'"' -f4 | cut -dT -f1)
+
 
 # Cant find  GitHub URL - KooL's dots version
-if [ -z "$github_version" ]; then
+if [ -z "$github_date" ]; then
+  notify-send -i "$iDIR/error.png" "ERROR "!"" "Unable to find upstream repo. exiting.... "
   exit 1
 fi
 
 # Comparing local and github versions
-if [ "$(echo -e "$github_version\n$local_version" | sort -V | head -n 1)" = "$github_version" ]; then
+if [[ ! "$github_date" > "$local_date" ]]; then
    notify-send -i "$iDIR/note.png" "KooL Hyprland:" "No update available"
   exit 0
 else
@@ -45,11 +46,10 @@ else
   			exit 1
 		fi
         kitty -e bash -c "
-          cd $KooL_Dots_DIR &&
+          cd ~/.config &&
           git stash &&
           git pull &&
-          ./copy.sh &&
-		  notify-send -u critical -i "$iDIR/ja.png" 'Update Completed:' 'Kindly log out and relogin to take effect'
+	  notify-send -u critical -i "$iDIR/ja.png" 'Update Completed:' 'Kindly log out and relogin to take effect'
         "
 	
       else
@@ -58,7 +58,7 @@ else
   			exit 1
 		fi
         kitty -e bash -c "
-          git clone --depth=1 https://github.com/JaKooLit/Hyprland-Dots.git $KooL_Dots_DIR &&
+          git clone --depth=1 https://github.com/GomezMig03/dotfiles.git $KooL_Dots_DIR &&
           cd $KooL_Dots_DIR &&
           chmod +x copy.sh &&
           ./copy.sh &&
