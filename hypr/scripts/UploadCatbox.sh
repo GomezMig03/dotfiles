db5fe665c6099415296a7f069a92b68488f1eeb7
Miguel <gomezmig03@gmail.com>
2026-04-04 20:01:26 +0200

    Add catbox icon

--- hypr/scripts/UploadCatbox.sh
+++ hypr/scripts/UploadCatbox.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 img=$(wl-paste -l | grep '^image/' | head -n1)
 txt=$(wl-paste -l | head -n1)
-if [[ ! -e "/tmp/litterico.png" ]]; then
-	wcurl "https://catbox.moe/pictures/logo.png" -o "/tmp/litterico.png"
+if [[ ! -e "/tmp/catb.png" ]]; then
+	wcurl "https://catbox.moe/pictures/logo.png" -o "/tmp/catb.png"
 fi
 
 if [[ -n $img ]]; then
@@ -11,13 +11,14 @@ if [[ -n $img ]]; then
 	wl-paste -t "$img" > "$path"
 	link=$(curl -F "reqtype=fileupload" -F "fileToUpload=@$path" https://catbox.moe/user/api.php)
 	wl-copy "$link"
-	notify-send "Catbox" "Se ha terminado la subida" --icon="/tmp/litterico.png"
+	echo "$link"
+	notify-send "Catbox" "Se ha terminado la subida" --icon="/tmp/catb.png"
 	rm "$path"
 elif [[ -n $txt ]]; then
 	path="/tmp/litter-$(date +%F%T).txt"
 	wl-paste > "$path"
 	link=$(curl -F "reqtype=fileupload" -F "fileToUpload=@$path" https://catbox.moe/user/api.php)
 	wl-copy "$link"
-	notify-send "Catbox" "Se ha terminado la subida" --icon="/tmp/litterico.png"
+	notify-send "Catbox" "Se ha terminado la subida" --icon="/tmp/catb.png"
 	rm "$path"
 fi
